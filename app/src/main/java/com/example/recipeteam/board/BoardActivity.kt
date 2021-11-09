d1bb9d99657cc8eb946fa832258932f581709c39
user <lilpkiah@gmail.com>
2021-11-09 19:58:43 +0900

    게시판, 게시글 작성 ui

--- app/src/main/java/com/example/recipeteam/board/BoardActivity.kt
+++ app/src/main/java/com/example/recipeteam/board/BoardActivity.kt
@@ -17,6 +17,8 @@ import org.w3c.dom.Text
 
 class BoardActivity : AppCompatActivity(), View.OnClickListener {
 
+    lateinit var boardAdapter: BoardAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_main)
