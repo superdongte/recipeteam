ac151c514c0096bbd6c6abb53cb3dd561b6669c6
user <lilpkiah@gmail.com>
2021-11-10 18:34:48 +0900

    ui

--- app/src/main/java/com/example/recipeteam/CookList.kt
+++ app/src/main/java/com/example/recipeteam/CookList.kt
@@ -1,4 +1,11 @@
 package com.example.recipeteam
 
-class CookList {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CookList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cook_receipe_list)
+    }
 }
