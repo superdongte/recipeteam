7dd66c5ca1285b84f72d8dfe0f9c3bf3ecbf73ee
user <lilpkiah@gmail.com>
2021-11-10 20:35:06 +0900

    ui

--- app/src/main/java/com/example/recipeteam/CookReceipeList.kt
+++ app/src/main/java/com/example/recipeteam/CookReceipeList.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class CookReceipeList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.cook_receipe_list)
+        setContentView(R.layout.recipe_all_lists)
     }
 }
