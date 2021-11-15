5a9b8caefffab8e24d1397d257de3d5167fdc06f
user <lilpkiah@gmail.com>
2021-11-15 21:47:10 +0900

    main ui

--- app/src/main/java/com/example/recipeteam/MainActivity.kt
+++ app/src/main/java/com/example/recipeteam/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.recipeteam
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.example.recipeteam.board.BoardActivity
 import com.example.recipeteam.recipe.CookListAcitivity
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+/*
         var btnReceipe=findViewById<Button>(R.id.btnCook)
         var btnBoard=findViewById<Button>(R.id.btnBoard)
         var savedId= intent.getStringExtra("savedId")
@@ -26,6 +27,14 @@ class MainActivity : AppCompatActivity() {
             val intent= Intent(this,BoardActivity::class.java)
             intent.putExtra("savedId", savedId)
             startActivity(intent)
-        }
+        }*/
+    }
+
+    fun onClick_boardbtn(view: View) {
+        startActivity(Intent(this, BoardActivity::class.java))
+    }
+
+    fun onClick_cookbtn(view: View) {
+        startActivity(Intent(this, CookListAcitivity::class.java))
     }
 }
